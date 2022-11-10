39cd1b6e41bb687a436b9ce59724a87edfc85a1d
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2022-11-10 22:12:10 +0100

    feat: add Homepage image full size

--- client/src/Components/Homepage.js
+++ client/src/Components/Homepage.js
@@ -1,11 +1,65 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+const HomepageContent = styled('section')(({ theme }) => ({
+    color: theme.palette.common.white,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+        height: '100vh',
+        minHeight: 'auto',
+        maxHeight: 'auto',
+    },
+}));
+
 function Homepage(props) {
+    const { children } = props;
 
     return (
-        <>
-            Homepage
-        </>
+        <Container className="container-full-size">
+            <HomepageContent>
+                <Container
+                    sx={{
+                        mt: 3,
+                        mb: 14,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    {children}
+                    <Box
+                        sx={{
+                            position: 'absolute',
+                            left: 0,
+                            right: 0,
+                            top: 0,
+                            bottom: 0,
+                            backgroundColor: 'common.black',
+                            opacity: 0.5,
+                            zIndex: -1,
+                        }}
+                    />
+                    <Box className='homepage-background' />
+                </Container>
+                <Button
+                    color="secondary"
+                    variant="contained"
+                    size="large"
+                    component="a"
+                    href=""
+                    sx={{ minWidth: 200 }}
+                >
+                    Register
+                </Button>
+            </HomepageContent>
+        </Container>
     );
-
 }
 
 export default Homepage
