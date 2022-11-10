7ac051ceb5e3d0183f6278169d06c7173291c769
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2022-11-10 11:54:37 +0100

    feat: add navbar style

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,107 +1,108 @@
 import { Outlet } from "react-router-dom";
 import * as React from 'react';
+import Container from '@mui/material/Container';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
+import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import Logo from "./Logo";
 
-const pages = ['Hikes'];
+const drawerWidth = 240;
+const navItems = ['Hikes'];
 
 function MyNavbar(props) {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
     };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Container>
+                <Container maxWidth="xs">
+                    <Logo />
+                </Container>
+            </Container>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
         <>
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        {/* Logo */}
-                        <Container sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} maxWidth="xs">
+            <Box sx={{ display: 'flex' }}>
+                {/* Full Navbar */}
+                <AppBar position="static" component="nav">
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Container sx={{ display: { xs: 'none', sm: 'flex' }, mr: 1, ml: 1 }} maxWidth="xs">
                             <Logo />
                         </Container>
-
-                        {/* Hamburger Menu */}
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: { xs: 'block', md: 'none' },
-                                }}
-                            >
-                                {pages.map((page) => (
-                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">{page}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-
-                        {/* Logo for Device*/}
-                        <Container sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} maxWidth="xs">
-                            <Logo />
-                        </Container>
-
-                        {/* Pages when not in the hamburger menu */}
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            {pages.map((page) => (
-                                <Button
-                                    key={page}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: 'white', display: 'block' }}
-                                >
-                                    {page}
+                        <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                            {navItems.map((item) => (
+                                <Button key={item} sx={{ color: '#fff' }}>
+                                    {item}
                                 </Button>
                             ))}
                         </Box>
-
-                        {/* Account User Buttons */}
-                        <Button color="inherit">Login</Button>
-                        <Button color="inherit">Register</Button>
+                        <Box sx={{ display: { xs: 'flex', sm: 'flex' } }} className="box-end">
+                            {/* Account User Buttons */}
+                            <Button variant="text" color="inherit" sx={{ mr: 2 }}>Login</Button>
+                            <Button variant="outlined" color="inherit">Register</Button>
+                        </Box>
                     </Toolbar>
-                </Container>
-            </AppBar >
+                </AppBar>
+
+                {/* Navbar on Device */}
+                <Box component="nav">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                        }}
+                        sx={{
+                            display: { xs: 'block', sm: 'none' },
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                </Box>
+            </Box>
             <Outlet />
         </>
+
     );
 
 }
