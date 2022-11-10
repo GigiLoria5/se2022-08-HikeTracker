bf41c52a07eee6150c9b65631651e3644da1496f
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2022-11-10 08:07:32 +0100

    feat: initial setup of the FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import MyNavbar from './Components/Navbar';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Root />
+    </Router>
+  )
+}
+
+function Root() {
+  return (
+    <Routes>
+      <Route path='/' element={<MyNavbar />}>
+        {/* Outlets */}
+        <Route path='' element={<h1>Homepage</h1>} />
+        <Route path='hikes' element={<><h1>Hikes List by Estelle</h1></>} />
+      </Route>
+
+      {/* The following routes will NOT have the navbar */}
+      <Route path='/login' element={<><h1>Login Page By Antonello</h1></>} />
+      <Route path='/register' element={<><h1>Register Page By Antonello</h1></>} />
+
+      <Route path='*' element={<><h1>Oh no! Page not found.</h1> <p>Return to our <Link to="/" >homepage</ Link>. </p></>} />
+    </Routes >
   );
 }
 
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+function MyNavbar(props) {
+
+    return (
+        <>
+            Navbar
+            <Outlet />
+        </>
+    );
+
+}
+
+export default MyNavbar
