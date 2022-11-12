4c72d5614002854f78f047be549dbfb5114c5a5b
michelepistan <82035230+michelepistan@users.noreply.github.com>
2022-11-12 01:54:19 +0100

    maps with multiple markers

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -1,67 +1,90 @@
 import { useEffect } from 'react';
 import { Grid } from '@mui/material';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Polygon, Polyline, TileLayer, FeatureGroup } from 'react-leaflet';
 import { Marker, Popup } from 'react-leaflet';
 import { useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
 
+import { Point, Trail, coordArray } from './Utils';
 
-export default function Map() {
+//test
+const p1 = new Point("P1", 45.06968, 7.70493);
+const p2 = new Point("P2", 45.05942, 7.80493);
+const p3 = new Point("P3", 45.06988, 7.90493);
+const p4 = new Point("P4", 45.06968, 7.70493);
 
-    useEffect(() => {
-        const L = require("leaflet");
-    
-        delete L.Icon.Default.prototype._getIconUrl;
-    
-        L.Icon.Default.mergeOptions({
-          iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-          iconUrl: require("leaflet/dist/images/marker-icon.png"),
-          shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-        });
-      }, []);
+const trail = new Trail(p1, p4, [p2, p3]);
 
-    const position = [45.06968, 7.70493];
+export default function Map(props) {
 
-    const outerBounds = [
-        [50.505, -29.09],
-        [52.505, 29.09],
-      ];
-/*
-      return (
-        <Map center={[51.505, -0.09]} zoom={13} style={{ height: "100vh" }}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
+  useEffect(() => {
+    const L = require("leaflet");
+
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    });
+
+  }, []);
+
+  const position = [45.06968, 7.70493];
+
+  //const positions = [[45.06968, 7.70493], [45.05942, 7.80493], [45.06988, 7.90493], [45.06968, 7.70493]];
+  const positions = [[45.06968, 7.70493], [45.05942, 7.80493]];
+
+  return (
+    <>
+
+      {/** */}
+      <MapContainer center={position} zoom={11} scrollWheelZoom={false} style={{ height: "50vh" }}>
+        
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+
+        <FeatureGroup>
+          {positions?.map((mark, i) => (
+            <Marker key={i} position={mark}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
-        </Map>
-      );
+          ))}
 
-*/
-
-    return(
-        <>
-
-            {/** */}
-            <MapContainer center={position} zoom={11} scrollWheelZoom={false} style={{ height: "50vh" }}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-            </MapContainer>
-            {/**/}
+          <Polyline positions={positions} color="red" />
+        </FeatureGroup>
 
 
+        <Polygon color="red" positions={positions} />
 
-        </>
-    )
+        <Marker position={p2.coord}>
+          <Popup>
+            {p2.label}
+          </Popup>
+        </Marker>
+
+        <Marker position={p3.coord}>
+          <Popup>
+            {p3.label}
+          </Popup>
+        </Marker>
+
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+      </MapContainer>
+      {/**/}
+
+
+
+    </>
+  )
 }
