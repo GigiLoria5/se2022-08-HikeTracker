6c65ab329f17a72b9d62c331b0fa8734f5078cb0
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2022-11-13 22:16:44 +0100

    fix: missing check on reference points

--- server/routes/Hike.js
+++ server/routes/Hike.js
@@ -21,15 +21,15 @@ const router = express.Router();
 //TODO: still need to handle login check
 router.post('/hikes', async (req, res) => {
     try {
-        if(!req.files) {
+        if (!req.files) {
             throw "no file uploaded"
         } else {
             //let name = req.body.name;
             const gpx = req.files.gpx;
             const name = Date.now() + "_" + gpx.name.replace(/\.[^/.]+$/, "");
-            
+
             const author_id = req.user && req.user.id;
-            
+
             const hike = new Hike(
                 req.body.title,
                 req.body.peak_altitude,
@@ -40,67 +40,89 @@ router.post('/hikes', async (req, res) => {
                 req.body.ascent,
                 req.body.track_length,
                 req.body.expected_time,
-                req.body.difficulty, 
-                name, 
-                req.body.start_point_type, 
-                req.body.start_point_id, 
-                req.body.end_point_type, 
+                req.body.difficulty,
+                name,
+                req.body.start_point_type,
+                req.body.start_point_id,
+                req.body.end_point_type,
                 req.body.end_point_id
             )
             let hike_id;
 
-            if(!hike.isValid()){
+            if (!hike.isValid()) {
                 throw "invalid arguments"
             }
-            switch(hike.start_point_type){
+            switch (hike.start_point_type) {
                 case "location":
-                    {const res = await locationDao.getLocationById(hike.start_point_id);
-                    if (res == null || res == undefined || res.length == 0){
-                        throw "invalid start point"
-                    }
-                    break};
+                    {
+                        const res = await locationDao.getLocationById(hike.start_point_id);
+                        if (res == null || res == undefined || res.length == 0) {
+                            throw "invalid start point"
+                        }
+                        break
+                    };
                 case "hut":
-                    {const res = await hutDao.getHutById(hike.start_point_id);
-                    if (res == null || res == undefined || res.length == 0){
-                        throw "invalid start point"
-                    }
-                    break};
+                    {
+                        const res = await hutDao.getHutById(hike.start_point_id);
+                        if (res == null || res == undefined || res.length == 0) {
+                            throw "invalid start point"
+                        }
+                        break
+                    };
                 case "parking_lot":
-                    {const res = await parkingDao.getParkingLotById(hike.start_point_id);
-                    if (res == null || res == undefined || res.length == 0){
-                        throw "invalid start point"
-                    }
-                    break};
+                    {
+                        const res = await parkingDao.getParkingLotById(hike.start_point_id);
+                        if (res == null || res == undefined || res.length == 0) {
+                            throw "invalid start point"
+                        }
+                        break
+                    };
                 default:
                     throw "invalid start point"
             }
-            switch(hike.end_point_type){
+            switch (hike.end_point_type) {
                 case "location":
-                    {const res = await locationDao.getLocationById(hike.end_point_id);
-                    if (res == null || res == undefined || res.length == 0){
-                        throw "invalid end point"
-                    }
-                    break};
+                    {
+                        const res = await locationDao.getLocationById(hike.end_point_id);
+                        if (res == null || res == undefined || res.length == 0) {
+                            throw "invalid end point"
+                        }
+                        break
+                    };
                 case "hut":
-                    {const res = await hutDao.getHutById(hike.end_point_id);
-                    if (res == null || res == undefined || res.length == 0){
-                        throw "invalid end point"
-                    }
-                    break};
+                    {
+                        const res = await hutDao.getHutById(hike.end_point_id);
+                        if (res == null || res == undefined || res.length == 0) {
+                            throw "invalid end point"
+                        }
+                        break
+                    };
                 case "parking_lot":
-                    {const res = await parkingDao.getParkingLotById(hike.end_point_id);
-                    if (res == null || res == undefined || res.length == 0){
-                        throw "invalid end point"
-                    }
-                    break};
+                    {
+                        const res = await parkingDao.getParkingLotById(hike.end_point_id);
+                        if (res == null || res == undefined || res.length == 0) {
+                            throw "invalid end point"
+                        }
+                        break
+                    };
                 default:
                     throw "invalid end point"
             }
+
+            /* Check types before adding hike */
+            const possibleTypes = ['location', 'hut', 'parking_lot'];
+            const refPointTypes = JSON.parse(req.body.reference_points).points.map((point) => point.type);
+            const checkRefPointTypes = refPointTypes.every((type) => possibleTypes.includes(type));
+            if (!checkRefPointTypes)
+                throw "invalid reference points";
+
+            /* Add hike after everything is correct */
             const id = await hikeDao.addHike(hike, author_id)
             hike_id = id;
-            for (const p of JSON.parse(req.body.reference_points).points){
+
+            for (const p of JSON.parse(req.body.reference_points).points) {
                 let res = null;
-                switch(p.type){
+                switch (p.type) {
                     case "location":
                         res = await locationDao.getLocationById(p.id);
                         break;
@@ -111,14 +133,14 @@ router.post('/hikes', async (req, res) => {
                         res = await parkingDao.getParkingLotById(p.id);
                         break;
                 }
-                if (res == null || res == undefined || res.length == 0){
+                if (res == null || res == undefined || res.length == 0) {
                     added = false;
                     hikeDao.deleteHike(id).then(_a => {
                         hikeDao.deleteReferencePoints(id);
                     });
                     throw "invalid reference points"
                 }
-                hikeDao.addReferencePoint(id, p.type, p.id).catch( err => {
+                hikeDao.addReferencePoint(id, p.type, p.id).catch(err => {
                     added = false;
                     hikeDao.deleteHike(id).then(_a => {
                         hikeDao.deleteReferencePoints(id);
@@ -126,7 +148,7 @@ router.post('/hikes', async (req, res) => {
                     throw "invalid reference points"
                 })
             }
-            if(gpx.mimetype!="application/gpx+xml") {
+            if (gpx.mimetype != "application/gpx+xml") {
                 hikeDao.deleteHike(hike_id).then(_a => {
                     hikeDao.deleteReferencePoints(hike_id);
                 });
@@ -134,7 +156,7 @@ router.post('/hikes', async (req, res) => {
             }
             //
             gpx.mv(`./gpx_files/${name}.gpx`, err => {
-                if (err){
+                if (err) {
                     hikeDao.deleteHike(hike_id).then(_a => {
                         hikeDao.deleteReferencePoints(hike_id);
                     });
--- server/test/TestHikeAdd.js
+++ server/test/TestHikeAdd.js
@@ -9,11 +9,11 @@ const app = require('../index.js');
 let agent = chai.request.agent(app);
 const expect = chai.expect;
 
-describe('Add hike',()=>{
+describe('Add hike', () => {
 
     let last_hike_id;
 
-    step('T1: POST/api/hikes [GOOD]', async function() {
+    step('T1: POST/api/hikes [GOOD]', async function () {
         await agent.post('/api/hikes')
             .set('content-type', 'multipart/form-data')
             .field("title", "Ring for Monte Calvo")
@@ -31,41 +31,39 @@ describe('Add hike',()=>{
             .field("end_point_type", "location")
             .field("end_point_id", 18)
             .field("reference_points", JSON.stringify({
-              "points": [
-                {
-                  "type":"hut", 
-                  "id":1
-                }, 
-                {
-                  "type":"hut", 
-                  "id":2
-                }, 
-                {
-                  "type":"location", 
-                  "id":12
-                }
-              ]
+                "points": [
+                    {
+                        "type": "hut",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
             }))
-            .attach('gpx','test/TestFileGood.gpx')
-            .then(function(res) {
+            .attach('gpx', 'test/TestFileGood.gpx')
+            .then(function (res) {
                 res.should.have.status(201);
             });
     });
-    
-    step('T1.5: Delete the previous hike', async function() {
+
+    step('T1.5: Delete the previous hike', async function () {
         const hikes = await hikeDao.getAllHikes();
-        console.log(hikes);
-        for(h of hikes){
-          last_hike_id = h.id;
+        for (h of hikes) {
+            last_hike_id = h.id;
         }
         hikeDao.deleteHike(last_hike_id).then(_a => {
             hikeDao.deleteReferencePoints(last_hike_id);
         });
         const hikes2 = await hikeDao.getAllHikes();
-        console.log(hikes2);
     });
 
-    step('T2: POST/api/hikes [NO FILE]', async function() {
+    step('T2: POST/api/hikes [NO FILE]', async function () {
         await agent.post('/api/hikes')
             .set('content-type', 'multipart/form-data')
             .field("title", "Ring for Monte Calvo")
@@ -83,66 +81,66 @@ describe('Add hike',()=>{
             .field("end_point_type", "location")
             .field("end_point_id", 18)
             .field("reference_points", JSON.stringify({
-              "points": [
-                {
-                  "type":"hut", 
-                  "id":1
-                }, 
-                {
-                  "type":"hut", 
-                  "id":2
-                }, 
-                {
-                  "type":"location", 
-                  "id":12
-                }
-              ]
+                "points": [
+                    {
+                        "type": "hut",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
             }))
-            .then(function(res) {
+            .then(function (res) {
                 res.should.have.status(500);
             });
     });
 
-    step('T3: POST/api/hikes [HIKE NOT VALID]', async function() {
-      await agent.post('/api/hikes')
-          .set('content-type', 'multipart/form-data')
-          // .field("title", undefined)
-          .field("peak_altitude", 1357)
-          .field("city", "Carignano")
-          .field("province", "Torino")
-          .field("country", "Italy")
-          .field("description", "It runs between ...")
-          .field("ascent", 320)
-          .field("track_length", 6.2)
-          .field("expected_time", 3.3)
-          .field("difficulty", 2)
-          .field("start_point_type", "parking_lot")
-          .field("start_point_id", 3)
-          .field("end_point_type", "location")
-          .field("end_point_id", 18)
-          .field("reference_points", JSON.stringify({
-            "points": [
-              {
-                "type":"hut", 
-                "id":1
-              }, 
-              {
-                "type":"hut", 
-                "id":2
-              }, 
-              {
-                "type":"location", 
-                "id":12
-              }
-            ]
-          }))
-          .attach('gpx','test/TestFileGood.gpx')
-          .then(function(res) {
-              res.should.have.status(500);
-          });
-  });
+    step('T3: POST/api/hikes [HIKE NOT VALID]', async function () {
+        await agent.post('/api/hikes')
+            .set('content-type', 'multipart/form-data')
+            // .field("title", undefined)
+            .field("peak_altitude", 1357)
+            .field("city", "Carignano")
+            .field("province", "Torino")
+            .field("country", "Italy")
+            .field("description", "It runs between ...")
+            .field("ascent", 320)
+            .field("track_length", 6.2)
+            .field("expected_time", 3.3)
+            .field("difficulty", 2)
+            .field("start_point_type", "parking_lot")
+            .field("start_point_id", 3)
+            .field("end_point_type", "location")
+            .field("end_point_id", 18)
+            .field("reference_points", JSON.stringify({
+                "points": [
+                    {
+                        "type": "hut",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
+            }))
+            .attach('gpx', 'test/TestFileGood.gpx')
+            .then(function (res) {
+                res.should.have.status(500);
+            });
+    });
 
-    step('T4: POST/api/hikes [START POINT TYPE NOT VALID]', async function() {
+    step('T4: POST/api/hikes [START POINT TYPE NOT VALID]', async function () {
         await agent.post('/api/hikes')
             .set('content-type', 'multipart/form-data')
             .field("title", "Ring for Monte Calvo")
@@ -160,141 +158,141 @@ describe('Add hike',()=>{
             .field("end_point_type", "location")
             .field("end_point_id", 18)
             .field("reference_points", JSON.stringify({
-              "points": [
-                {
-                  "type":"hut", 
-                  "id":1
-                }, 
-                {
-                  "type":"hut", 
-                  "id":2
-                }, 
-                {
-                  "type":"location", 
-                  "id":12
-                }
-              ]
+                "points": [
+                    {
+                        "type": "hut",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
             }))
-            .attach('gpx','test/TestFileGood.gpx')
-            .then(function(res) {
+            .attach('gpx', 'test/TestFileGood.gpx')
+            .then(function (res) {
                 res.should.have.status(500);
             });
     });
 
-    step('T5: POST/api/hikes [END POINT TYPE NOT VALID]', async function() {
-      await agent.post('/api/hikes')
-          .set('content-type', 'multipart/form-data')
-          .field("title", "Ring for Monte Calvo")
-          .field("peak_altitude", 1357)
-          .field("city", "Carignano")
-          .field("province", "Torino")
-          .field("country", "Italy")
-          .field("description", "It runs between ...")
-          .field("ascent", 320)
-          .field("track_length", 6.2)
-          .field("expected_time", 3.3)
-          .field("difficulty", 2)
-          .field("start_point_type", "parking_lot")
-          .field("start_point_id", 3)
-          .field("end_point_type", "ciao")
-          .field("end_point_id", 18)
-          .field("reference_points", JSON.stringify({
-            "points": [
-              {
-                "type":"hut", 
-                "id":1
-              }, 
-              {
-                "type":"hut", 
-                "id":2
-              }, 
-              {
-                "type":"location", 
-                "id":12
-              }
-            ]
-          }))
-          .attach('gpx','test/TestFileGood.gpx')
-          .then(function(res) {
-              res.should.have.status(500);
-          });
-  });
+    step('T5: POST/api/hikes [END POINT TYPE NOT VALID]', async function () {
+        await agent.post('/api/hikes')
+            .set('content-type', 'multipart/form-data')
+            .field("title", "Ring for Monte Calvo")
+            .field("peak_altitude", 1357)
+            .field("city", "Carignano")
+            .field("province", "Torino")
+            .field("country", "Italy")
+            .field("description", "It runs between ...")
+            .field("ascent", 320)
+            .field("track_length", 6.2)
+            .field("expected_time", 3.3)
+            .field("difficulty", 2)
+            .field("start_point_type", "parking_lot")
+            .field("start_point_id", 3)
+            .field("end_point_type", "ciao")
+            .field("end_point_id", 18)
+            .field("reference_points", JSON.stringify({
+                "points": [
+                    {
+                        "type": "hut",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
+            }))
+            .attach('gpx', 'test/TestFileGood.gpx')
+            .then(function (res) {
+                res.should.have.status(500);
+            });
+    });
 
-  step('T6: POST/api/hikes [REFERENCE POINT TYPE NOT VALID]', async function() {
-      await agent.post('/api/hikes')
-          .set('content-type', 'multipart/form-data')
-          .field("title", "Ring for Monte Calvo")
-          .field("peak_altitude", 1357)
-          .field("city", "Carignano")
-          .field("province", "Torino")
-          .field("country", "Italy")
-          .field("description", "It runs between ...")
-          .field("ascent", 320)
-          .field("track_length", 6.2)
-          .field("expected_time", 3.3)
-          .field("difficulty", 2)
-          .field("start_point_type", "parking_lot")
-          .field("start_point_id", 3)
-          .field("end_point_type", "location")
-          .field("end_point_id", 18)
-          .field("reference_points", JSON.stringify({
-            "points": [
-              {
-                "type":"hello", 
-                "id":1
-              }, 
-              {
-                "type":"hut", 
-                "id":2
-              }, 
-              {
-                "type":"location", 
-                "id":12
-              }
-            ]
-          }))
-          .attach('gpx','test/TestFileGood.gpx')
-          .then(function(res) {
-              res.should.have.status(500);
-          });
-  });
+    step('T6: POST/api/hikes [REFERENCE POINT TYPE NOT VALID]', async function () {
+        await agent.post('/api/hikes')
+            .set('content-type', 'multipart/form-data')
+            .field("title", "Ring for Monte Calvo")
+            .field("peak_altitude", 1357)
+            .field("city", "Carignano")
+            .field("province", "Torino")
+            .field("country", "Italy")
+            .field("description", "Goosebump...")
+            .field("ascent", 320)
+            .field("track_length", 6.2)
+            .field("expected_time", 3.3)
+            .field("difficulty", 2)
+            .field("start_point_type", "parking_lot")
+            .field("start_point_id", 3)
+            .field("end_point_type", "location")
+            .field("end_point_id", 18)
+            .field("reference_points", JSON.stringify({
+                "points": [
+                    {
+                        "type": "hello",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
+            }))
+            .attach('gpx', 'test/TestFileGood.gpx')
+            .then(function (res) {
+                res.should.have.status(500);
+            });
+    });
 
-  step('T7: POST/api/hikes [FILE NOT VALID]', async function() {
-      await agent.post('/api/hikes')
-          .set('content-type', 'multipart/form-data')
-          .field("title", "Ring for Monte Calvo")
-          .field("peak_altitude", 1357)
-          .field("city", "Carignano")
-          .field("province", "Torino")
-          .field("country", "Italy")
-          .field("description", "It runs between ...")
-          .field("ascent", 320)
-          .field("track_length", 6.2)
-          .field("expected_time", 3.3)
-          .field("difficulty", 2)
-          .field("start_point_type", "parking_lot")
-          .field("start_point_id", 3)
-          .field("end_point_type", "location")
-          .field("end_point_id", 18)
-          .field("reference_points", JSON.stringify({
-            "points": [
-              {
-                "type":"hut", 
-                "id":1
-              }, 
-              {
-                "type":"hut", 
-                "id":2
-              }, 
-              {
-                "type":"location", 
-                "id":12
-              }
-            ]
-          }))
-          .attach('gpx','test/TestFileWrong.txt')
-          .then(function(res) {
-              res.should.have.status(500);
-          });
-  });
+    step('T7: POST/api/hikes [FILE NOT VALID]', async function () {
+        await agent.post('/api/hikes')
+            .set('content-type', 'multipart/form-data')
+            .field("title", "Ring for Monte Calvo")
+            .field("peak_altitude", 1357)
+            .field("city", "Carignano")
+            .field("province", "Torino")
+            .field("country", "Italy")
+            .field("description", "It runs between ...")
+            .field("ascent", 320)
+            .field("track_length", 6.2)
+            .field("expected_time", 3.3)
+            .field("difficulty", 2)
+            .field("start_point_type", "parking_lot")
+            .field("start_point_id", 3)
+            .field("end_point_type", "location")
+            .field("end_point_id", 18)
+            .field("reference_points", JSON.stringify({
+                "points": [
+                    {
+                        "type": "hut",
+                        "id": 1
+                    },
+                    {
+                        "type": "hut",
+                        "id": 2
+                    },
+                    {
+                        "type": "location",
+                        "id": 12
+                    }
+                ]
+            }))
+            .attach('gpx', 'test/TestFileWrong.txt')
+            .then(function (res) {
+                res.should.have.status(500);
+            });
+    });
 });
