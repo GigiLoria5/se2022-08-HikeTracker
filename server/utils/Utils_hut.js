27f5dfda08d9d0a9d58827046080b38e9719eeae
FedeBel <101119750+FedeBel@users.noreply.github.com>
2022-12-06 17:03:22 +0100

    fix: some errors

--- server/utils/Utils_hut.js
+++ server/utils/Utils_hut.js
@@ -1,10 +1,8 @@
-
-//It returns all the informations about a hut/parking_lot/location from the type and the id
+//It returns the huts applying hut type filters
 async function handleHutType(huts, hut_type, hutTypes) {
     if (Array.isArray(hut_type)) {
         let temp = [];
         for (let type of hut_type) {
-            console.log(type);
             if (!hutTypes.includes(type)) {
                 return -1
             }
@@ -21,15 +19,17 @@ async function handleHutType(huts, hut_type, hutTypes) {
     }
 }
 
+//It returns the huts applying range filters
 async function handleRangeFilters(result, rangeFilters) {
     for (const key in rangeFilters) {
         if (rangeFilters[key][0] && rangeFilters[key][1]) {
             if (rangeFilters[key][0] < 0.0 || rangeFilters[key][1] < 0.0) {
                 return -1;
             }
-            return result.filter(h => h[key] >= rangeFilters[key][0] && h[key] <= rangeFilters[key][1]);
+            result = result.filter(h => h[key] >= rangeFilters[key][0] && h[key] <= rangeFilters[key][1]); 
         }
     }
+    return result
 }
 
 module.exports = { handleHutType, handleRangeFilters };
