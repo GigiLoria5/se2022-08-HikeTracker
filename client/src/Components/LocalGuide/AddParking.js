fc00232594a526c7a1211cd119bd7180ad0612b2
michelepistan <82035230+michelepistan@users.noreply.github.com>
2022-12-09 15:17:32 +0100

    fix: fe style

--- client/src/Components/LocalGuide/AddParking.js
+++ client/src/Components/LocalGuide/AddParking.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Button from "@mui/material/Button";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Autocomplete from '@mui/material/Autocomplete';
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import { TextField } from '@mui/material';
+import { createTheme, ThemeProvider, Divider, TextField } from '@mui/material';
 import { Link, useNavigate } from "react-router-dom";
 import Stack from '@mui/material/Stack';
 import { getCountries, getProvincesByCountry, getCitiesByProvince } from '../../Utils/GeoData'
 import Alert from '@mui/material/Alert';
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import MapLocator from '../Map/MapLocator';
 import { floatInputSanitizer } from '../../Utils/InputSanitizer';
@@ -176,10 +176,10 @@ function AddParking() {
 
 
     const thm = {
+        marginBottom: 1,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        fontWeight: 600,
     };
 
 
@@ -188,110 +188,138 @@ function AddParking() {
             <Grid container >
 
                 <ThemeProvider theme={theme} >
+
                     <Grid xs={12}>
-                        <Typography variant="h5" marginTop={2}  sx={thm} align={'center'}>
+                        <Typography variant="h5" marginTop={2} sx={thm} align={'center'}>
                             ADD PARKING LOT
                         </Typography>
                     </Grid>
-                    <Grid xs={0} md={2}></Grid>
+                    <Grid xs={0} md={3}></Grid>
+                    <Grid xs={12} md={6} marginTop={3} >
+                        <Container component="main" maxWidth="m">
+                            <Paper elevation={3} sx={{ ...thm }} >
 
-                    <Grid xs={12} md={8} marginTop={3} >
-                        <Paper elevation={3} sx={{ mb: 4 }} >
-                            
-                                <Typography variant="h5" sx={thm} margin="normal" fontWeight={550}  marginBottom={2}>
+                                <Typography variant="h5" sx={thm} margin="normal" fontWeight={550} marginTop={1}>
                                     Specify a position
                                 </Typography>
 
-                                
-                                <Typography variant="h6" sx={{ fontSize: 14, ...thm }} color="grey.700">
-                                    Click on the map to define a parking lot
+
+                                <Typography sx={{ fontSize: 14 }} color="grey.700">
+                                    Click on the map or type the coordinates to define a parking lot
                                 </Typography>
-                                
 
-
-                            <Grid container>
-                                {/*MAP*/}
-                                <Box component="div" width={"100%"} align='center' marginTop={1}>
-                                    <Box>
-                                        <MapLocator position={position} setPosition={setPosition} radius={null} height={'200px'} width={'500px'} initialLat={initialLat} initialLng={initialLng} zoomLevel={zoomLevel} />
-                                    </Box>
-                                </Box>
-
-                                <Grid xs={12} sx={thm} >
-                                    <Stack direction='row' justifyContent="center" alignItems="center" marginTop={2}>
-                                        <TextField required variant="outlined" color='primary' label="Latitude" sx={{ width: '13ch', m: 1, mb: { xs: 0, sm: 1 } }} value={position ? `${position.lat}` : ""} onChange={(e) => handleChangeLat(e.target.value)}  />
-                                        <TextField required variant="outlined" color='primary' label="Longitude" sx={{ width: '13ch', m: 1, mb: { xs: 0, sm: 1 } }} value={position ? `${position.lng}` : ""} onChange={(e) => handleChangeLng(e.target.value)}  />
-                                    </Stack>
-
-                                    < Stack direction='column'  align='center'>
-                                        {/*COUNTRY FIELD*/}
-                                        <Autocomplete
-                                            required
-                                            disablePortal
-                                            id="combo-box-demo"
-                                            options={countries}
-                                            value={country!==""? country : null}
-                                            sx={{ m: 1, width: '28ch', pt: { xs: 0, md: 1.1 } }}
-                                            onChange={(e, value) => {
-                                                e.preventDefault();
-                                                setCountry(value); setProvince(''); setCity(''); setAddress('');
-                                                reset();
-                                            }}
-                                            renderInput={(params) => <TextField required {...params} label="Country" error={formValues.country.error} helperText={formValues.country.error && formValues.country.errorMessage} />}
-                                        />
-                                        {/*PROVINCE FIELD*/}
-                                        <Autocomplete
-                                            required
-                                            disabled={!(country)}
-                                            disablePortal
-                                            id="combo-box-demo2"
-                                            value={province!==""? province : null}
-                                            options={provinces}
-                                            key={country}
-                                            sx={{ m: 1, width: '28ch' }}
-                                            onChange={(e, value) => {
-                                                e.preventDefault();
-                                                setProvince(value); setCity(''); setAddress('');
-                                                reset();
-                                            }}
-                                            renderInput={(params) => <TextField {...params} required label="Province" error={formValues.province.error} helperText={formValues.province.error && formValues.province.errorMessage} />}
-                                        />
-                                        {/*CITY FIELD*/}
-                                        <Autocomplete
-                                            required
-                                            disabled={!(country && province)}
-                                            disablePortal
-                                            id="combo-box-demo3"
-                                            options={cities}
-                                            key={[province, country]}
-                                            sx={{ m: 1, width: '28ch' }}
-                                            onChange={(e, value) => {
-                                                e.preventDefault();
-                                                setCity(value); setAddress('');
-                                                reset();
-                                            }}
-                                            value={city!==""? city: null}
-                                            renderInput={(params) => <TextField {...params} required label="City" error={formValues.city.error} helperText={formValues.city.error && formValues.city.errorMessage} />}
-                                        />
-                                        <TextField variant="outlined" required color='primary' label="Address" sx={{ width: '28ch', m: 1, mb: 1 }} value={address} onChange={(e) => setAddress(e.target.value)} />
-                                        <TextField variant="outlined" required color='primary'  InputProps={{ inputProps: { min: "0", step: "1" } }} label="Capacity" sx={{ width: '28ch', m: 1, mb: 1 }} type="number" value={capacity} onChange={(e) => setCapacity(e.target.value)} />
-
-                                    </Stack>
-                                    {message && <Alert sx={{ m: 1, width: 'fit-content', align: 'center'}} severity="error" onClose={() => setMessage('')}>{message}</Alert>}
-
+                                {/* MAP */}
+                                <Grid xs={0} sm={1}></Grid>
+                                <Grid xs={12} sm={10} >
+                                    <MapLocator position={position} setPosition={setPosition} radius={null} height={'50vh'} width={'100'} initialLat={initialLat} initialLng={initialLng} zoomLevel={zoomLevel} />
                                 </Grid>
-                            </Grid>
+                                <Grid xs={0} sm={1}></Grid>
+
+                                <Grid container>
+
+                                    {/* coordinates */}
+                                    <Grid xs={12} sx={thm}>
+                                        <Typography align='center' variant="h6" fontWeight={520} margin={2} marginBottom={0}>
+                                            COORDINATES
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid xs={12} sx={{ ...thm, mb: 2 }} >
+
+                                        {/*LATITUDE FIELD*/}
+                                        <TextField margin="normal" variant="outlined" sx={{ width: '30ch', maxWidth: '30ch', marginTop: 1 }} required label="Latitude" type="number" value={position ? `${position.lat}` : ""} onChange={(e) => handleChangeLat(e.target.value)} />
+
+                                        {/*LONGITUDE FIELD*/}
+                                        <TextField margin="normal" variant="outlined" sx={{ width: '30ch', maxWidth: '30ch', marginTop: 1 }} required label="Longitude" type="number" value={position ? `${position.lng}` : ""} onChange={(e) => handleChangeLng(e.target.value)} />
+
+                                    </Grid>
+
+                                    <Divider variant="middle" />
+
+                                    <Grid xs={12} sx={thm}>
+                                        <Typography align='center' variant="h6" fontWeight={520} margin={2} marginBottom={0}>
+                                            GEOGRAPHIC INFORMATION
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid xs={12} sx={thm} >
+
+                                        < Stack direction='column' align='center'>
+                                            {/*COUNTRY FIELD*/}
+                                            <Autocomplete
+                                                required
+                                                disablePortal
+                                                id="combo-box-demo"
+                                                options={countries}
+                                                sx={{ width: '30ch', maxWidth: '30ch', m: 1 }}
+                                                onChange={(e, value) => {
+                                                    e.preventDefault();
+                                                    setCountry(value); setProvince(''); setCity('')
+                                                }}
+                                                renderInput={(params) => <TextField required {...params} label="Country" />}
+                                            />
+                                            {/*PROVINCE FIELD*/}
+                                            <Autocomplete
+                                                required
+                                                disabled={!(country)}
+                                                disablePortal
+                                                id="combo-box-demo2"
+                                                options={provinces}
+                                                key={country}
+                                                sx={{ width: '30ch', maxWidth: '30ch', m: 1 }}
+                                                onChange={(e, value) => {
+                                                    e.preventDefault();
+                                                    setProvince(value); setCity('')
+                                                }}
+                                                renderInput={(params) => <TextField {...params} required label="Province" />}
+                                            />
+                                            {/*CITY FIELD*/}
+                                            <Autocomplete
+                                                required
+                                                disabled={!(country && province)}
+                                                disablePortal
+                                                id="combo-box-demo3"
+                                                options={cities}
+                                                key={[province, country]}
+                                                sx={{ width: '30ch', maxWidth: '30ch', m: 1 }}
+                                                onChange={(e, value) => {
+                                                    e.preventDefault();
+                                                    setCity(value);
+                                                }}
+                                                renderInput={(params) => <TextField {...params} required label="City" />}
+                                            />
+                                            <TextField variant="outlined" required color='primary' label="Address" sx={{ width: '30ch', maxWidth: '30ch', m: 1 }} value={address} onChange={(e) => setAddress(e.target.value)} />
+
+                                        </Stack>
+
+                                        <Divider variant="middle" />
+
+                                        <Grid xs={12} sx={thm}>
+                                            <Typography align='center' variant="h6" fontWeight={520} margin={2} marginBottom={0}>
+                                                PARKING LOT INFORMATION
+                                            </Typography>
+                                        </Grid>
+
+                                        <TextField variant="outlined" required color='primary' InputProps={{ inputProps: { min: "0", step: "1" } }} label="Capacity" sx={{ width: '30ch', maxWidth: '30ch', m: 1 }} type="number" value={capacity} onChange={(e) => setCapacity(e.target.value)} />
+
+                                        {message && <Alert sx={{ m: 1, width: 'fit-content', align: 'center' }} severity="error" onClose={() => setMessage('')}>{message}</Alert>}
+
+                                    </Grid>
+                                </Grid>
 
 
 
-                            {/****************************************************SUBMIT BUTTONS********************************************************/}
-                            <Stack direction="row" justifyContent="center" alignItems="center">
-                                <Button sx={{ m: 1, mb: 4, mt: 2, minWidth: '80px' }} component={Link} to={"/local-guide-page"} variant="outlined" color='error'>CANCEL</Button>
-                                <Button sx={{ m: 1, mb: 4, mt: 2, minWidth: '80px' }} onClick={handleSubmission} variant="contained" color='primary'>ADD PARKING LOT</Button>
-                            </Stack>
+                                {/****************************************************SUBMIT BUTTONS********************************************************/}
+                                <Stack direction="row" justifyContent="center" alignItems="center">
+                                    <Button sx={{ m: 1, mb: 4, mt: 4, minWidth: '80px' }} component={Link} to={"/"} variant="outlined" color='error'>CANCEL</Button>
+                                    <Button sx={{ m: 1, mb: 4, mt: 4, minWidth: '80px' }} onClick={handleSubmission} variant="contained" color='primary'>ADD PARKING LOT</Button>
+                                </Stack>
 
-                        </Paper>
+                            </Paper>
+
+                        </Container>
                     </Grid>
+                    <Grid xs={0} md={3}></Grid>
                 </ThemeProvider>
 
             </Grid >
