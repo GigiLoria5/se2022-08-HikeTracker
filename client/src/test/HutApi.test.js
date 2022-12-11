5e66ff80d9a3a94ffee3d4094d63befd3aa550fc
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2022-12-11 14:38:48 +0100

    fix: wrong test

--- client/src/test/HutApi.test.js
+++ client/src/test/HutApi.test.js
@@ -10,87 +10,87 @@ import { isPointInsideRange } from '../Utils/GeoUtils';
 const APIURL = 'http://localhost:3001';
 
 describe('frontend test: hut creation', () => {
-    
+
     let h0 = new Hut({
-        id:undefined,
-        name:"Hut 0",
-        city:"Turin",
-        province:"TO",
-        country:"Italy",
-        address:"Corso Duca, 12",
-        altitude:"200",
-        description:"Description of the hut",
-        beds_number:12,
-        opening_period:undefined,
-        longitude:"12.3456",
-        latitude:"65.4321",
-        phone_number:"3456789012",
-        email:"hut0@hut.it",
-        website:"www.hut1.com",
-        type:"hiking_hut"
+        id: undefined,
+        name: "Hut 0",
+        city: "Turin",
+        province: "TO",
+        country: "Italy",
+        address: "Corso Duca, 12",
+        altitude: "200",
+        description: "Description of the hut",
+        beds_number: 12,
+        opening_period: undefined,
+        longitude: "12.3456",
+        latitude: "65.4321",
+        phone_number: "3456789012",
+        email: "hut0@hut.it",
+        website: "www.hut1.com",
+        type: "hiking_hut"
     });
 
     let h1 = new Hut({
-        id:undefined,
-        name:"Hut 1",
-        city:"Aosta",
-        province:"AO",
-        country:"Italy",
-        address:"Via dei matti, 0",
-        altitude:"42",
-        description:"Era una casa molto carina",
-        beds_number:0,
-        opening_period:undefined,
-        longitude:"65.4321",
-        latitude:"12.3456",
-        phone_number:"",
-        email:"",
-        website:"",
-        type:"unmanaged_hut"
+        id: undefined,
+        name: "Hut 1",
+        city: "Aosta",
+        province: "AO",
+        country: "Italy",
+        address: "Via dei matti, 0",
+        altitude: "42",
+        description: "Era una casa molto carina",
+        beds_number: 0,
+        opening_period: undefined,
+        longitude: "65.4321",
+        latitude: "12.3456",
+        phone_number: "3456789012",
+        email: "hut0@hut.it",
+        website: "",
+        type: "unmanaged_hut"
     });
 
     let h2 = new Hut({
-        id:undefined,
-        name:"Hut 2",
-        city:"Turin",
-        province:"TO",
-        country:"Italy",
-        address:"Corso Duca, 12",
-        altitude:"200",
-        description:"Description of the hut",
-        beds_number:12,
-        opening_period:undefined,
-        longitude:"12.3456",
-        latitude:"65.4321",
-        phone_number:"3456789012",
-        email:"hut2@hut.it",
-        website:"www.hut1.com",
-        type:""
+        id: undefined,
+        name: "Hut 2",
+        city: "Turin",
+        province: "TO",
+        country: "Italy",
+        address: "Corso Duca, 12",
+        altitude: "200",
+        description: "Description of the hut",
+        beds_number: 12,
+        opening_period: undefined,
+        longitude: "12.3456",
+        latitude: "65.4321",
+        phone_number: "3456789012",
+        email: "hut2@hut.it",
+        website: "www.hut1.com",
+        type: ""
     });
 
     let h3 = new Hut({
-        id:undefined,
-        name:"Hut 3",
-        city:"Turin",
-        province:"TO",
-        country:"Italy",
-        address:"Corso Duca, 12",
-        altitude:"200",
-        description:"Description of the hut",
-        beds_number:12,
-        opening_period:undefined,
-        longitude:12.3456,
-        latitude:65.4321,
-        phone_number:"3456789012",
-        email:"hut2@hut.it",
-        website:"www.hut1.com",
-        type:"hiking_hut"
+        id: undefined,
+        name: "Hut 3",
+        city: "Turin",
+        province: "TO",
+        country: "Italy",
+        address: "Corso Duca, 12",
+        altitude: "200",
+        description: "Description of the hut",
+        beds_number: 12,
+        opening_period: undefined,
+        longitude: 12.3456,
+        latitude: 65.4321,
+        phone_number: "3456789012",
+        email: "hut2@hut.it",
+        website: "www.hut1.com",
+        type: "hiking_hut"
     });
 
     it('setup', async () => {
-        const user = await API.logIn({username:"g.desantis@localguide.it", password:"password"});
+        const user = await API.logIn({ username: "g.desantis@localguide.it", password: "password" });
     })
-      
+
     it('T0: correct body', async () => {
         const res = await API.addHut(h0);
         expect(res).toBe(true);
@@ -102,49 +102,49 @@ describe('frontend test: hut creation', () => {
     })
 
     it('T2: missing mandatory field', async () => {
-        try{
+        try {
             await API.addHut(h2);
         }
-        catch (err){
+        catch (err) {
             expect(err);
-        }  
+        }
     })
 
     it('T3: wrong-type field', async () => {
-        try{
+        try {
             await API.addHut(h3);
         }
-        catch (err){
+        catch (err) {
             expect(err);
-        } 
+        }
     })
 
-    it('T4: delete huts', async() =>{
+    it('T4: delete huts', async () => {
         try {
             await API.deleteHut("Hut 0");
-        } catch (err){
+        } catch (err) {
             console.log(err);
-        } 
+        }
     })
 
-    it('T5: delete huts', async() =>{
+    it('T5: delete huts', async () => {
         try {
             await API.deleteHut("Hut 1");
-        } catch (err){
+        } catch (err) {
             console.log(err);
-        } 
+        }
     })
 });
 
 describe('frontend test: get hut by radius', () => {
 
     it('setup', async () => {
-        const user = await API.logIn({username:"g.desantis@localguide.it", password:"password"});
+        const user = await API.logIn({ username: "g.desantis@localguide.it", password: "password" });
     })
-      
+
     it('T0: GOOD', async () => {
         const res = await API.getHutsByRadius(44.5, 7, 100000);
-        var huts = res.filter((r) => {return isPointInsideRange({ latitude: 44.5, longitude: 7 }, 100000, { latitude: r.latitude, longitude: r.longitude })});
+        var huts = res.filter((r) => { return isPointInsideRange({ latitude: 44.5, longitude: 7 }, 100000, { latitude: r.latitude, longitude: r.longitude }) });
         expect(res).toStrictEqual(huts);
     })
 
--- client/src/test/ParkingApi.test.js
+++ client/src/test/ParkingApi.test.js
@@ -10,109 +10,114 @@ import { isPointInsideRange } from '../Utils/GeoUtils';
 const APIURL = 'http://localhost:3001';
 
 describe('frontend test: hut creation', () => {
-    
+
     const p0 = new Parking({
-        id:undefined,
-        city:"Turin",
-        province:"Turin",
-        country:"Italy",
-        latitude:"0",
-        longitude:"12.3456", 
-        address:"Corso Vittorio Emanuele II",
-        capacity:100});
+        id: undefined,
+        city: "Turin",
+        province: "Turin",
+        country: "Italy",
+        latitude: "0",
+        longitude: "12.3456",
+        address: "Corso Vittorio Emanuele II",
+        capacity: 100
+    });
     const p1 = new Parking({
-        id:undefined,
-        city:"Turin",
-        province:"Turin",
-        country:"Italy",
-        latitude:"12.0000",
-        longitude:"65.4321", 
-        address:"",
-        capacity:100}); //missing address
+        id: undefined,
+        city: "Turin",
+        province: "Turin",
+        country: "Italy",
+        latitude: "12.0000",
+        longitude: "65.4321",
+        address: "",
+        capacity: 100
+    }); //missing address
     const p2 = new Parking({
-        id:undefined,
-        city:"",
-        province:"Turin",
-        country:"Italy",
-        latitude:"12.0000",
-        longitude:"65.4321", 
-        address:"Corso Castelfidardo",
-        capacity:100}); //missing city
+        id: undefined,
+        city: "",
+        province: "Turin",
+        country: "Italy",
+        latitude: "12.0000",
+        longitude: "65.4321",
+        address: "Corso Castelfidardo",
+        capacity: 100
+    }); //missing city
     const p3 = new Parking({
-        id:undefined,
-        city:"",
-        province:"",
-        country:"Italy",
-        latitude:"12.0000",
-        longitude:"65.4321", 
-        address:"Corso Castelfidardo",
-        capacity:100}); //missing city and province
+        id: undefined,
+        city: "",
+        province: "",
+        country: "Italy",
+        latitude: "12.0000",
+        longitude: "65.4321",
+        address: "Corso Castelfidardo",
+        capacity: 100
+    }); //missing city and province
     const p4 = new Parking({
-        id:undefined,
-        city:"",
-        province:"",
-        country:"",
-        latitude:"12.0000",
-        longitude:"65.4321", 
-        address:"Corso Castelfidardo",
-        capacity:100}); //missing city, province and country
-    
+        id: undefined,
+        city: "",
+        province: "",
+        country: "",
+        latitude: "12.0000",
+        longitude: "65.4321",
+        address: "Corso Castelfidardo",
+        capacity: 100
+    }); //missing city, province and country
+
     it('setup user session', async () => {
-        const user = await API.logIn({username:"g.desantis@localguide.it", password:"password"});
+        const user = await API.logIn({ username: "g.desantis@localguide.it", password: "password" });
     })
-      
+
     it('T0: correct body', async () => {
         const res = await API.addParking(p0);
         expect(res).toBe(true);
     })
 
     it('T1: missing address', async () => {
-        try{
+        try {
             const res = await API.addParking(p1);
         }
-        catch (err){
+        catch (err) {
             expect(err);
             console.log(err);
-        }  
+        }
     })
 
     it('T2: missing city', async () => {
-        try{
+        try {
             const res = await API.addParking(p2);
         }
-        catch (err){
+        catch (err) {
             expect(err);
             console.log(err);
-        }  
+        }
     })
 
     it('T3: missing city and province', async () => {
-        try{
+        try {
             const res = await API.addParking(p3);
         }
-        catch (err){
+        catch (err) {
             expect(err);
             console.log(err);
-        }  
+        }
     })
 
     it('T4: missing city, province and country', async () => {
-        try{
+        try {
             const res = await API.addParking(p4);
         }
-        catch (err){
+        catch (err) {
             expect(err);
             console.log(err);
-        }  
+        }
     })
 
     it('delete stuff created', async () => {
-        try{
+        try {
             await API.deleteParking(p0.address);
         }
-        catch (err){
+        catch (err) {
             console.log(err);
-        } 
+        }
     })
 
 });
@@ -120,12 +125,12 @@ describe('frontend test: hut creation', () => {
 describe('frontend test: get parking lot by radius', () => {
 
     it('setup', async () => {
-        const user = await API.logIn({username:"g.desantis@localguide.it", password:"password"});
+        const user = await API.logIn({ username: "g.desantis@localguide.it", password: "password" });
     })
-      
+
     it('T0: GOOD', async () => {
         const res = await API.getParkingsByRadius(44.5, 7, 100000);
-        var huts = res.filter((r) => {return isPointInsideRange({ latitude: 44.5, longitude: 7 }, 100000, { latitude: r.latitude, longitude: r.longitude })});
+        var huts = res.filter((r) => { return isPointInsideRange({ latitude: 44.5, longitude: 7 }, 100000, { latitude: r.latitude, longitude: r.longitude }) });
         expect(res).toStrictEqual(huts);
     })
 
